bee9acefbc417a02e1693591bb01d2ec9b8ceead
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-01-14 21:59:26 +0630

    environment variables

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-const url =
-  "mongodb+srv://myatsumon2531999:3A6XEPXQrzPtvWLU@cluster0.1qr1wh2.mongodb.net/?retryWrites=true&w=majority";
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(url);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Database connection failed");
