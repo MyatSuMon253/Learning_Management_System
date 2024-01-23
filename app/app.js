04351d08133eef7661a9bfffa2044b952b4ae415
msm <myatsumon@ucsy.edu.mm>
2024-01-23 21:59:09 +0630

    Feat: custom middleware

--- app/app.js
+++ app/app.js
@@ -7,6 +7,38 @@ const app = express();
 // middleware
 app.use(morgan("dev"));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+let user = {
+  name: "John Doe",
+  isAdmin: false,
+  isLogin: true,
+};
+
+const isLogin = (req, res, next) => {
+  if(user.isLogin) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (user.isAdmin) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized, You are not admin",
+    });
+  }
+};
+
+app.use(isLogin, isAdmin)
 
 // admin register
 app.use("/api/v1/admins", adminRouter);
